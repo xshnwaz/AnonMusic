46a6b785893860f53cea3266bcba5a8cfaebfeea
xshnwaz <xshnwaz@gmail.com>
2026-04-11 01:16:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ DURATION_LIMIT_MIN = validate_env_var("DURATION_LIMIT", cast_type=int, default=3
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Vibe-Bots/AnonMusic")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/xshnwaz/AnonMusic")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv("GIT_TOKEN")
 
